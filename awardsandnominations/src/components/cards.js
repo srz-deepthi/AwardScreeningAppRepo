96282b043737e3a3f279c2f2cbf7067ec62088aa
srz-deepthi <deepthik@smartrabbitz.com>
2022-04-07 22:41:43 +0530

    voting userInfo

--- awardsandnominations/src/components/cards.js
+++ awardsandnominations/src/components/cards.js
@@ -10,6 +10,21 @@ import { red } from '@mui/material/colors';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import Button from '@mui/material/Button';
 import { Link } from 'react-router-dom'
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+import TextField from '@mui/material/TextField'
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
 
 const ExpandMore = styled((props) => {
 
@@ -26,6 +41,12 @@ const ExpandMore = styled((props) => {
 
 export default function Cards(props) {
 
+    //modal
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+  
+    //toggle flag
   const [flagId,setFlag] = React.useState({
         fId:"",
         fExp:false,
@@ -83,12 +104,47 @@ export default function Cards(props) {
                 </Collapse>:""}
               </Card>
               </div> ))}
-              <Button variant="contained" size="large" sx ={{ mx: 80 , width: 150 , color: "white"}}> 
+              {/* <Button variant="contained" size="large" sx ={{ mx: 80 , width: 150 , color: "white"}}> 
               <Link to="/user" style={{ textDecoration: 'none' }}>
                         Vote
               </Link>
-              </Button>
+              </Button> */}
+              <div>
+          <Button variant="contained" size="large" sx ={{ mx: 80 , width: 150 , color: "white"}} onClick={handleOpen}> VOTE </Button>
+          <Modal
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+          >
+            <Box sx={style}>
+              <h2 style={{ color:'#505050'}}>User Details</h2>
+              <div style={{ padding: 10 }}>
+                <div style={{ padding: 10 }}>
+                <TextField
+                  label="Name"
+                  id="outlined-size-small"
+                  size="small"
+                />
+                </div>
+                <div style={{ padding: 10 }}>
+                <TextField
+                  label="Email"
+                  id="outlined-size-small"
+                  size="small"
+                  type="email"
+                />
+                </div>
+                <div style={{ padding: 10 }}>
+                  <Button variant="contained" sx ={{ mx:2,width: 150 , color: "white" }} onClick={handleClose}>Cancel</Button>
+                  <Button variant="contained" sx ={{ width: 150 , color: "white"}}>OK</Button>
+                </div>
+              </div>
+            </Box>
+          </Modal>
+        </div>
       </>
   );
 }
+{/* <Button onClick={handleOpen}>Open modal</Button> */}
 
